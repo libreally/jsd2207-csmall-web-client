15f86508918c0c41c936942e9816dd3d619d6862
Administrator <2359304261@qq.com>
2022-11-17 15:29:44 +0800

    v4

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios';
 import qs from 'qs';
+import wangEditor from 'wangeditor';
 
+Vue.prototype.wangEditor=wangEditor;
 Vue.prototype.qs = qs;
 Vue.prototype.axios = axios;
 Vue.use(ElementUI);
--- src/views/sys-admin/product/SpuAddNewStep4View.vue
+++ src/views/sys-admin/product/SpuAddNewStep4View.vue
@@ -10,23 +10,37 @@
 
     <el-divider></el-divider>
 
-    <el-descriptions :title="this.ruleForm.name">
-      <el-descriptions-item label="品牌" span="1">
-        <el-tag size="small">{{this.ruleForm.brandName}}</el-tag>
-      </el-descriptions-item>
-      <el-descriptions-item label="类别" span="2">{{this.ruleForm.categoryName}}</el-descriptions-item>
-      <el-descriptions-item label="标题" span="3">{{this.ruleForm.title}}</el-descriptions-item>
-      <el-descriptions-item label="简介" span="3">{{this.ruleForm.description}}</el-descriptions-item>
-      <el-descriptions-item label="相册" span="3">{{this.ruleForm.albumName}}</el-descriptions-item>
-      <el-descriptions-item label="货号">{{this.ruleForm.typeNumber}}</el-descriptions-item>
-      <el-descriptions-item label="售价">{{this.ruleForm.listPrice}}</el-descriptions-item>
-      <el-descriptions-item label="计件单位">{{this.ruleForm.unit}}</el-descriptions-item>
-      <el-descriptions-item label="库存">{{this.ruleForm.stock}}</el-descriptions-item>
-      <el-descriptions-item label="库存阈警值">{{this.ruleForm.stockThreshold}}</el-descriptions-item>
-      <el-descriptions-item label="排序序号">{{this.ruleForm.sort}}</el-descriptions-item>
-      <el-descriptions-item label="关键词">{{this.ruleForm.keywords}}</el-descriptions-item>
-      <el-descriptions-item label="标签">{{this.ruleForm.tags}}</el-descriptions-item>
-    </el-descriptions>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="基本信息">
+        <el-descriptions :title="this.ruleForm.name">
+          <el-descriptions-item label="品牌" span="1">
+            <el-tag size="small">{{this.ruleForm.brandName}}</el-tag>
+          </el-descriptions-item>
+          <el-descriptions-item label="类别" span="2">{{this.ruleForm.categoryName}}</el-descriptions-item>
+          <el-descriptions-item label="标题" span="3">{{this.ruleForm.title}}</el-descriptions-item>
+          <el-descriptions-item label="简介" span="3">{{this.ruleForm.description}}</el-descriptions-item>
+          <el-descriptions-item label="相册" span="3">{{this.ruleForm.albumName}}</el-descriptions-item>
+          <el-descriptions-item label="货号">{{this.ruleForm.typeNumber}}</el-descriptions-item>
+          <el-descriptions-item label="售价">{{this.ruleForm.listPrice}}</el-descriptions-item>
+          <el-descriptions-item label="计件单位">{{this.ruleForm.unit}}</el-descriptions-item>
+          <el-descriptions-item label="库存">{{this.ruleForm.stock}}</el-descriptions-item>
+          <el-descriptions-item label="库存阈警值">{{this.ruleForm.stockThreshold}}</el-descriptions-item>
+          <el-descriptions-item label="排序序号">{{this.ruleForm.sort}}</el-descriptions-item>
+          <el-descriptions-item label="关键词">{{this.ruleForm.keywords}}</el-descriptions-item>
+          <el-descriptions-item label="标签">{{this.ruleForm.tags}}</el-descriptions-item>
+        </el-descriptions>
+      </el-form-item>
+      <el-form-item label="商品详情">
+        <div id="wangEditor"></div>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="warning" @click="backToPreStep()">上一步</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')">下一步</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+
   </div>
 </template>
 
@@ -34,13 +48,16 @@
 export default {
   data() {
     return {
+      editor: {},
       ruleForm: {
+      },
+      rules: {
       }
     };
   },
   methods: {
     backToPreStep() {
-      this.$router.push('spu-add-new2');
+      this.$router.push('spu-add-new3');
     },
     loadLocalRuleForm() {
       let localRuleFormString = localStorage.getItem('ruleForm');
@@ -49,8 +66,27 @@ export default {
         this.ruleForm = localRuleForm;
       }
     },
+    initWangEditor() {
+      this.editor = new this.wangEditor('#wangEditor');
+      this.editor.create();
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.ruleForm.detail=this.editor.txt.html();
+          console.log(this.ruleForm);
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
   },
   mounted() {
+    this.initWangEditor();
     this.loadLocalRuleForm();
   }
 }
